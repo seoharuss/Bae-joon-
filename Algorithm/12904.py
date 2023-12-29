591e36d5bba2f19cba333072619e155e0b4a13c5
InKi Park <park28525@gmail.com>
2023-12-29 15:02:41 +0900

    문제 풀이 완료 + 느낀점 추가

--- Algorithm/12904.py
+++ Algorithm/12904.py
@@ -1,26 +1,22 @@
-from collections import deque
-
-def reverse(string):
-  reversed_string = string[::-1]
-  return reversed_string
-
-S = input()
-T = input()
-
-queue = deque([S])
-count = 1
-while len(queue[0]) != len(T):
-  temp = queue.popleft()
-  first = temp+'A'
-  second = reverse(temp)+'B'
-  queue.append(first)
-  queue.append(second)
-  count += 1
+S = list(input())
+T = list(input())
 
 result = 0
-for i in queue:
-  if T == i:
+while T:
+  if T[-1] == 'A':
+    T.pop()
+  elif T[-1] == 'B':
+    T.pop()
+    T.reverse()
+  if S == T:
     result = 1
+    break
 print(result)
 
 # 1차 제출 : 메모리 초과
+# 2차 생각 : 역으로 생각하기 
+# 문자열 뒤에 A를 추가한다 -> 문자열 마지막에 A가 나오면 A를 삭제한다.
+# 문자열을 뒤집고 뒤에 B를 추가한다 -> 문자열을 뒤집고 뒤에 B가 나오면 B를 삭제한다.
+
+# S를 T로 만들 생각만 계속 했는데, 생각을 역으로 발상하는 것도 해봐야겠다고 생각했다.
+# 이 문제의 핵심은 T를 S로 바꾸는 것이 핵심이다.
