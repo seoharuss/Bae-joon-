05840a2b6a0680459a94ef572b2a11fdeebf819d
InKi Park <park28525@gmail.com>
2024-01-05 15:59:54 +0900

    다 풀었는데 틀림

---
+++ Algorithm/1461.py
@@ -0,0 +1,36 @@
+# 책의 개수 N, 한 번에 들 수 있는 책의 개수 M
+# N개의 책의 위치 주어짐
+
+N, M = map(int, input().split())
+location = list(map(int, input().split()))
+
+positive = []
+negative = []
+
+for i in range(N):
+  if location[i] < 0:
+    negative.append(location[i])
+  else:
+    positive.append(location[i])
+    
+negative.sort()
+positive.sort(reverse=True)
+temp = 0
+if -negative[0] < positive[-1]:
+  temp = positive[-1]
+else:
+  temp = negative[0]
+
+result = 0
+for i in range(0, len(negative), M):
+  if negative[i] != temp:
+    result += (-negative[i])*2
+for k in range(0, len(positive), M):
+  if positive[k] != temp:
+    result += positive[k]*2
+
+if temp < 0:
+  result += -temp
+else:
+  result += temp
+print(result)
