c1d8fd2e6b437cd128e2e135165f0bc966499b03
InKi Park <park28525@gmail.com>
2024-01-07 11:10:40 +0900

    첫번째 풀이 완료

--- Algorithm/2579.py
+++ Algorithm/2579.py
@@ -3,5 +3,13 @@ stairPoint = []
 
 for i in range(stairCount):
   stairPoint.append(int(input()))
-  
-print(stairPoint)
+
+dp = [0]*stairCount
+dp[0] = stairPoint[0]
+dp[1] = stairPoint[0]+stairPoint[1]
+dp[2] = max(dp[0]+stairPoint[2], stairPoint[1]+stairPoint[2])
+
+for i in range(3, stairCount):
+  dp[i] = max(dp[i-2]+stairPoint[i], dp[i-3]+stairPoint[i-1]+stairPoint[i])
+
+print(dp[stairCount-1])
