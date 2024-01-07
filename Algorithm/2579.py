30186ac3ecc6cf9be159212602252dde32eeba87
InKi Park <park28525@gmail.com>
2024-01-07 11:13:37 +0900

    정답 완료

--- Algorithm/2579.py
+++ Algorithm/2579.py
@@ -1,15 +1,14 @@
 stairCount = int(input())
-stairPoint = []
+stairPoint = [0] * 301
+for i in range(1, stairCount + 1):
+    stairPoint[i] = int(input())
 
-for i in range(stairCount):
-  stairPoint.append(int(input()))
+dp = [0]*301
+dp[1] = stairPoint[1]
+dp[2] = stairPoint[1]+stairPoint[2]
+dp[3] = max(dp[1]+stairPoint[3], stairPoint[2]+stairPoint[3])
 
-dp = [0]*stairCount
-dp[0] = stairPoint[0]
-dp[1] = stairPoint[0]+stairPoint[1]
-dp[2] = max(dp[0]+stairPoint[2], stairPoint[1]+stairPoint[2])
-
-for i in range(3, stairCount):
+for i in range(4, stairCount+1):
   dp[i] = max(dp[i-2]+stairPoint[i], dp[i-3]+stairPoint[i-1]+stairPoint[i])
 
-print(dp[stairCount-1])
+print(dp[stairCount])
