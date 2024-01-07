c7f910cf139149adfa454047dab8bca9c8456745
InKi Park <park28525@gmail.com>
2024-01-07 11:15:19 +0900

    느낀점 추가

--- Algorithm/2579.py
+++ Algorithm/2579.py
@@ -12,3 +12,11 @@ for i in range(4, stairCount+1):
   dp[i] = max(dp[i-2]+stairPoint[i], dp[i-3]+stairPoint[i-1]+stairPoint[i])
 
 print(dp[stairCount])
+
+#########################################################3
+# dp로 접근하는 문제
+# 연속3번 계단을 오르면 안되므로 max값을 계산할 때
+# i-1번째에서 오는 값은 i-3의 값에서 i-1과 i를 더한다.
+# i-2번째에서 오는 값은 해당 값에 현재 i를 더한다.
+
+# dp는 부분으로 나누어서 생각하는 것이 중요한 것 같다.
