d98b073d61b19424512729d9ba4717ddddfd233e
InKi Park <park28525@gmail.com>
2024-01-09 15:36:22 +0900

    첫번째 풀이 실패

--- Algorithm/9465.py
+++ Algorithm/9465.py
@@ -3,4 +3,23 @@ testCase = int(input())
 for _ in range(testCase):
   n = int(input())
   score = [list(map(int, input().split())) for _ in range(2)]
-  print(score)
+  
+  dp = [0 for _ in range(n)]
+  temp = 0
+  if score[0][0] > score[1][0]:
+    dp[0] = score[0][0]
+    temp = 1
+  else:
+    dp[0] = score[1][0]
+    temp = 0
+    
+  for i in range(1, n-1):
+    if score[temp][i]+score[not temp][i+1] < score[temp][i+1]:
+      dp[i+1] = dp[i-1]+score[temp][i+1]
+      temp = not temp
+    else:
+      dp[i] = dp[i-1]+score[temp][i]
+      temp = not temp
+  if dp[n-1] == 0:
+    dp[n-1] = dp[n-2] + score[temp][n-1]
+  print(dp[n-1])
