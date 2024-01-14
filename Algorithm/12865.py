edc39bd4655be3a208b474e79c2366bf8695b37d
InKi Park <park28525@gmail.com>
2024-01-14 13:19:57 +0900

    코드완성

--- Algorithm/12865.py
+++ Algorithm/12865.py
@@ -1,16 +1,19 @@
 N, K = map(int, input().split())
-items = []
+items = [[0,0]]
+dp = [[0 for _ in range(K+1)] for _ in range(N+1)]
 
-
-for i in range(N):
-  w, v = map(int, input().split())
-  items.append((w, v))
-
-dp = [0 for _ in range(K+1)]
-
-
-
-
+for _ in range(N):
+  items.append(list(map(int, input().split())))
+  
+for i in range(1, N+1):
+  for j in range(1, K+1):
+    weight = items[i][0]
+    value = items[i][1]
+    if j < weight:
+      dp[i][j] = dp[i-1][j]
+    else:
+      dp[i][j] = max(value + dp[i-1][j-weight], dp[i-1][j])
+print(dp[N][K])
 #################################################
 # 첫번째 생각 (그리디 알고리즘)
 # 알고리즘 수업때 배웠었던 개념을 그대로 적용하려고 해봄
