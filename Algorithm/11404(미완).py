dd88af9084cdf9c4ea7c1b4469992cfd4feede8b
InKi Park <park28525@gmail.com>
2024-02-17 17:34:53 +0900

    시간초과로 오류

--- Algorithm/11404(미완).py
+++ Algorithm/11404(미완).py
@@ -1,22 +1,31 @@
-cities = int(input())
-bus = int(input())
+#입력
+n = int(input())
+m = int(input())
+bus_cost = [[100001 for _ in range(n+1)] for _ in range(n+1)]
 
-graph = [[0 for _ in range(cities+1)] for _ in range(cities+1)]
+for _ in range(m):
+    start, end, cost = map(int, input().split())
+    bus_cost[start][end] = min(cost, bus_cost[start][end])
 
-# 행 = 시작, 열 = 도착, 행렬값 = 비용
-for i in range(1, bus+1):
-  start, finish, cost_temp = map(int, input().split())
-  if graph[start][finish] == 0:
-    graph[start][finish] = cost_temp
-  else:
-    graph[start][finish] = min(cost_temp, graph[start][finish])
+#플로이드-워셜 알고리즘
+for k in range(1, n+1): #경로 for문이 가장 상위 단계여야 누락되지 않는다
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if i == j: #자기 자신으로 오는 경우는 없다고 했으므로
+                bus_cost[i][j] = 0 
+            else: #경로 거치는 것 or 직접 가는 것 or 이전 경로들
+                bus_cost[i][j] = min(bus_cost[i][j],
+                                     bus_cost[i][k] + bus_cost[k][j])
 
-for k in range(1, cities+1):
-  for i in range(1, cities+1):
-    for j in range(1, cities+1):
-      graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
-      
-print(graph)
+
+#출력
+for row in bus_cost[1:]:
+    for col in row[1:]:
+        if col == 100001:
+            print(0, end = " ")
+        else:
+            print(col, end = " ")
+    print()
 
 ##############################################################
 # 플로이드-워셜 알고리즘
